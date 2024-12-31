26df1095fb8a2e08a4b10fce35af8dbaa112a9c0
Hassan <mhtariq366@gmail.com>
2024-12-31 16:59:53 +0500

    fastapi subjects using enum

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class AvailableSubjects(str, Enum):
+    medical = 'pre-medical'
+    engineering = 'pre-engineering'
+    arts = 'arts'
+
 subjects = {
-    'pre-medical': ["Biology", "Chemistry", "Physics"],
-    'pre-engineering': ["Maths", "Chemistry", "Physics"],
+    'medical': ["Biology", "Chemistry", "Physics"],
+    'engineering': ["Maths", "Chemistry", "Physics"],
     'arts' : ["English", "History"]
 }
 
 @app.get("/get_subject/{name}")
-async def get_subject(name):
-    if name not in subjects.keys():
-        return f'Please select only from {subjects.keys()}'
+async def get_subject(name: AvailableSubjects):
     return subjects.get(name)
 
